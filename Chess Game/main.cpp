c4bc4cd7398b90cbce2828e2caa3f1943af1eb54
Ryan <ryanbarouki@gmail.com>
2020-07-24 17:09:03 +0100

    Initial Commit

---
+++ Chess Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess Game
+//
+//  Created by Ryan Barouki on 24/07/2020.
+//  Copyright © 2020 Ryan Barouki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
