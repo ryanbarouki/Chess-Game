c1577fc7c09e85345c1ae9d2e40b5a268e84b962
Ryan <ryanbarouki@gmail.com>
2020-07-26 21:11:24 +0100

    It now loads an image of a board. Added some comments to explain the SFML stuff.

--- Chess Game/main.cpp
+++ Chess Game/main.cpp
@@ -9,24 +9,40 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window( sf::VideoMode(640, 480), "Chess");
+    sf::RenderWindow window( sf::VideoMode(1000, 1000), "Chess");
+    // create a board rectangle
+    sf::RectangleShape board(sf::Vector2f(1000.0f, 1000.0f));
+    // center the position of the board
+    board.setPosition(0.0f, 0.0f);
     
-    while( window.isOpen()){
-        sf::Event event;
+    // create a texture so we can load an image of a chess board
+    sf::Texture boardTexture;
+    // load image of chess board
+    boardTexture.loadFromFile("/Users/ryanbarouki/Documents/Coding Projects/Chess Game/Chess Game/images/board.png");
+    // set the image as the texture of board by reference
+    board.setTexture(&boardTexture);
+    
+    
+    while(window.isOpen()){
         
-        while (window.pollEvent(event)){
-            switch (event.type){
+        sf::Event evnt;
+        
+        // allows us to close the window
+        while (window.pollEvent(evnt)){
+            // a switch statement for event types e.g close window, resize, input text etc.
+            switch (evnt.type){
+                    // close the window
                 case sf::Event::Closed:
                     window.close();
-                    
                     break;
             }
+            
         }
-        // Eleanor is here to commit
-        // Ryan added a comment
         window.clear();
-        // draw objects here
+        // draw the board here
+        window.draw(board);
         window.display();
     }
     
+    return 0;
 }
