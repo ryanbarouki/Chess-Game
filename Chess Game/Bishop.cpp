a10f9a06dd7ad684f03389c6009e237166c08a3e
Eleanor Warren <eleanorvwarren@gmail.com>
2020-07-28 23:00:12 +0100

    Did basic cpp files for all pieces

--- Chess Game/Bishop.cpp
+++ Chess Game/Bishop.cpp
@@ -1,1 +1,14 @@
 #include "Bishop.hpp"
+
+Bishop::Bishop(sf::Vector2f original_position,sf::Vector2f scale, int rect_x, int rect_y){
+    //piece.setSize(sf::Vector2f(777.0f,250.0f));
+    piece.setScale(scale);
+    piece.setPosition(original_position);
+    pieceTexture.loadFromFile("/Users/ryanbarouki/Documents/Coding Projects/Chess Game/Chess Game/images/pieces.png");
+    piece.setTexture(pieceTexture);
+    piece.setTextureRect(sf::IntRect(rect_x*180,rect_y*180,180,180));
+}
+ 
+void Bishop::Move(sf::Event event){
+    
+}
