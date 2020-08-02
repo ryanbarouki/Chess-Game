140bc5d0092561de5371938aeb2b203e6f35c523
Eleanor Warren <eleanorvwarren@gmail.com>
2020-08-02 20:57:56 +0100

    Move validation for Knight and Rook pieces

--- Chess Game/Knight.cpp
+++ Chess Game/Knight.cpp
@@ -8,7 +8,18 @@ Knight::Knight(sf::Vector2f original_position, int rect_x, int rect_y, char clr)
  
 bool Knight::canMoveTo(int row, int col, Board &board){
     bool valid = false;
-    // validation code here
+    // validation code
+    int col_orig = int((piece.getPosition().x-(size/2))/size);
+    int row_orig = int((piece.getPosition().y-(size/2))/size);
+    if (abs(row-row_orig)==1 && abs(col-col_orig)==2 && board.getPiece(row,col)==nullptr) {
+        valid = true;
+    } else if (abs(row-row_orig)==2 && abs(col-col_orig)==1 && board.getPiece(row,col)==nullptr) {
+        valid = true;
+    } else if (abs(row-row_orig)==1 && abs(col-col_orig)==2 && board.getPiece(row,col)!=nullptr) {
+        if (board.getPiece(row,col)->getColour()!= getColour()) {
+            valid = true;
+        }
+    }
     return valid;
 }
 
--- Chess Game/Rook.cpp
+++ Chess Game/Rook.cpp
@@ -8,6 +8,31 @@ Rook::Rook(sf::Vector2f original_position, int rect_x, int rect_y, char clr): Pi
 
 bool Rook::canMoveTo(int row, int col, Board &board){
     bool valid = false;
-    // validation code here
+    // validation code
+    int col_orig = int((piece.getPosition().x-(size/2))/size);
+    int row_orig = int((piece.getPosition().y-(size/2))/size);
+    if (row_orig==row or col_orig==col){
+        if (board.getPiece(row,col)==nullptr or board.getPiece(row,col)->getColour()!= getColour()) {
+            if (row_orig==row) {
+                for (int c=col_orig; c<=col; c++){
+                    if (board.getPiece(row,c)==nullptr){
+                        valid = true;
+                    } else {
+                        valid = false;
+                        break;
+                    }
+                }
+            } else if (col_orig==col) {
+                for (int r=row_orig; r<=row; r++){
+                    if (board.getPiece(r,col)==nullptr){
+                        valid = true;
+                    } else {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
     return valid;
 }
