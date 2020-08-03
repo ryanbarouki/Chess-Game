0d6b06b8758052b3ad12044f566e20a3986232e4
Eleanor Warren <eleanorvwarren@gmail.com>
2020-08-03 17:12:03 +0100

    King move validation

--- Chess Game/King.cpp
+++ Chess Game/King.cpp
@@ -8,6 +8,16 @@ King::King(sf::Vector2f original_position, int rect_x, int rect_y, char clr): Pi
  
 bool King::canMoveTo(int row, int col, Board &board){
     bool valid = false;
+    
     // validation code here
+    int col_final = int((piece.getPosition().x)/size);
+    int row_final = int((piece.getPosition().y)/size);
+    if ((abs(row-row_final)==1 && col==col_final) || (row==row_final && abs(col-col_final)==1) || (abs(row-row_final)==1 && abs(col-col_final)==1)) {
+        if(board.getPiece(row_final,col_final) == nullptr){
+            valid = true;
+        } else if (board.getPiece(row_final,col_final)->getColour()!= getColour()){
+                valid = true;
+        }
+    }
     return valid;
 }
