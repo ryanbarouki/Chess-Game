fbe45f1823f412cb788a40f25eff2ffdf0fbaf4a
Ryan <ryanbarouki@gmail.com>
2020-08-03 11:42:00 +0100

    Rook validation works

--- Chess Game/Rook.cpp
+++ Chess Game/Rook.cpp
@@ -9,22 +9,29 @@ Rook::Rook(sf::Vector2f original_position, int rect_x, int rect_y, char clr): Pi
 bool Rook::canMoveTo(int row, int col, Board &board){
     bool valid = false;
     // validation code
-    int col_orig = int((piece.getPosition().x-(size/2))/size);
-    int row_orig = int((piece.getPosition().y-(size/2))/size);
-    if (row_orig==row or col_orig==col){
-        if (board.getPiece(row,col)==nullptr or board.getPiece(row,col)->getColour()!= getColour()) {
-            if (row_orig==row) {
-                for (int c=col_orig; c<=col; c++){
-                    if (board.getPiece(row,c)==nullptr){
+    int col_final = int((piece.getPosition().x)/size);
+    int row_final = int((piece.getPosition().y)/size);
+    
+    if (row_final == row or col_final == col){
+        if (board.getPiece(row_final,col_final) == nullptr || board.getPiece(row_final,col_final)->getColour()!= getColour()) {
+            if (row_final == row) {
+                int col_min = std::min(col, col_final);
+                int col_max = std::max(col, col_final);
+                valid = true;
+                for (int c = col_min+1; c < col_max; c++){
+                    if (board.getPiece(row,c) == nullptr){
                         valid = true;
                     } else {
                         valid = false;
                         break;
                     }
                 }
-            } else if (col_orig==col) {
-                for (int r=row_orig; r<=row; r++){
-                    if (board.getPiece(r,col)==nullptr){
+            } else if (col_final == col) {
+                int row_min = std::min(row, row_final);
+                int row_max = std::max(row, row_final);
+                valid = true;
+                for (int r = row_min+1; r < row_max; r++){
+                    if (board.getPiece(r,col) == nullptr){
                         valid = true;
                     } else {
                         valid = false;
