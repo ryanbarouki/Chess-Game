02e97e1d17a45ff10950057599855593a1395423
Eleanor Warren <eleanorvwarren@gmail.com>
2020-08-03 17:29:12 +0100

    half-done queen validation

--- Chess Game/Queen.cpp
+++ Chess Game/Queen.cpp
@@ -10,5 +10,49 @@ Queen::Queen(sf::Vector2f original_position, int rect_x, int rect_y, char clr): 
 bool Queen::canMoveTo(int row, int col, Board &board){
     bool valid = false;
     // validation code here
+    int col_final = int((piece.getPosition().x)/size);
+    int row_final = int((piece.getPosition().y)/size);
+    if ((col==col_final) || (row==row_final) || (abs(row-row_final)==abs(col-col_final))) {
+        if (board.getPiece(row_final,col_final) == nullptr || board.getPiece(row_final,col_final)->getColour()!= getColour()) {
+            if (row_final == row) {
+                int col_min = std::min(col, col_final);
+                int col_max = std::max(col, col_final);
+                valid = true;
+                for (int c = col_min+1; c < col_max; c++){
+                    if (board.getPiece(row,c) == nullptr){
+                        valid = true;
+                    } else {
+                        valid = false;
+                        break;
+                    }
+                }
+            } else if (col_final == col) {
+                int row_min = std::min(row, row_final);
+                int row_max = std::max(row, row_final);
+                valid = true;
+                for (int r = row_min+1; r < row_max; r++){
+                    if (board.getPiece(r,col) == nullptr){
+                        valid = true;
+                    } else {
+                        valid = false;
+                        break;
+                    }
+                }
+            } else {
+                int col_min = std::min(col, col_final);
+                int col_max = std::max(col, col_final);
+                valid = true;
+                for (int c = col_min+1; c < col_max; c++){
+                    if (board.getPiece(row,c) == nullptr){
+                        valid = true;
+                    } else {
+                        valid = false;
+                        break;
+                    }
+                }
+                
+            }
+        }
+    }
     return valid;
 }
