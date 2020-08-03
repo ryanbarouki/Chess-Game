2080dd5f33242c4dbccd70404b87a56891c4c01a
Ryan <ryanbarouki@gmail.com>
2020-08-03 17:27:33 +0100

    Bishop validation

--- Chess Game/Bishop.cpp
+++ Chess Game/Bishop.cpp
@@ -9,6 +9,40 @@ Bishop::Bishop(sf::Vector2f original_position, int rect_x, int rect_y, char clr)
  
 bool Bishop::canMoveTo(int row, int col, Board &board){
     bool valid = false;
-    // validation code here
+    // validation code
+    int col_final = int((piece.getPosition().x)/size);
+    int row_final = int((piece.getPosition().y)/size);
+    
+    if(abs(col_final - col) == abs(row_final - row)){
+        if(board.getPiece(row_final, col_final) == nullptr || board.getPiece(row_final,col_final)->getColour()!= getColour()){
+            int col_min = std::min(col, col_final);
+            int col_max = std::max(col, col_final);
+            valid = true;
+            int row_min = std::min(row, row_final);
+            int row_max = std::max(row, row_final);
+            if((row_final == row_max && col_final == col_max) || (row_final == row_min && col_final == col_min)){
+                for (int c = col_min+1; c < col_max; c++){
+                        int r = (c - col_min) + row_min;
+                        if (board.getPiece(r, c) == nullptr){
+                            valid = true;
+                        } else {
+                            valid = false;
+                            break;
+                    }
+                }
+            } else {
+                for (int c = col_min+1; c < col_max; c++){
+                        int r = (col_min - c) + row_max;
+                        if (board.getPiece(r, c) == nullptr){
+                            valid = true;
+                        } else {
+                            valid = false;
+                            break;
+                    }
+                }
+            }
+        
+        }
+    }
     return valid;
 }
